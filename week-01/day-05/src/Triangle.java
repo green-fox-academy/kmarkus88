b8aff46e7880a6e9bcf68a37da8f4bc222180667
kmarkus88 <kimmelmarkus88@gmail.com>
2018-05-11 13:37:26 +0200

    FizzBuzz correction and Triangle

---
+++ week-01/day-05/src/Triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a number!");
+        int number = scanner.nextInt();
+
+        for (int row = 0; row < number+1; row++) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
+
