d89cd2d0be804de45bbbd3b2412c77344512a596
kmarkus88 <kimmelmarkus88@gmail.com>
2018-05-16 15:15:20 +0200

    GuessTheNumber

--- week-01/day-05/src/GuessTheNumber.java
+++ week-01/day-05/src/GuessTheNumber.java
@@ -1,28 +1,38 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-
     public static void main(String[] args) {
+        // Write a program that stores a number, and the user has to figure it out.
+        // The user can input guesses, after each guess the program would tell one
+        // of the following:
+        // The stored number is higher
+        // The stored number is lower
+        // You found the number: 8
 
+        System.out.println("Give me a max number! The numbers are always positive.");
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Give me a maximum random number! (my minimum will be 0)");
         int max = scanner.nextInt();
-        int secretNumber = (int) (Math.random() * (max+1));
 
-        System.out.println("Try to guess the number!");
-        System.out.println("What's the number?");
+        Random rand = new Random();
+        int  n = rand.nextInt(max) + 0;
+
+        System.out.println("Lets try it out!");
+
         int guess = scanner.nextInt();
 
-        while (secretNumber != guess) {
-            if (guess > secretNumber) {
-                System.out.println("The stored number is lower, guess again!");
+        while (n != guess) {
+            if (guess > n) {
+                System.out.println("The stored number is lower");
                 guess = scanner.nextInt();
-            } else if (guess < secretNumber) {
-                System.out.println("The stored number is higher, guess again!");
+            } else if (guess < n) {
+                System.out.println("The stored number is higher");
                 guess = scanner.nextInt();
             }
+
         }
-        System.out.println("You found the number: " + secretNumber);
+        System.out.println("You found the number: " + n);
     }
+
 }
+
