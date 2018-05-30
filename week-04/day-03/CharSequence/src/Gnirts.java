47c9e132b52e1b5a385045fd6c7137da56118dc1
kmarkus88 <kimmelmarkus88@gmail.com>
2018-05-30 16:22:08 +0200

    CharSequence: main added + shifter skeleton

--- week-04/day-03/CharSequence/src/Gnirts.java
+++ week-04/day-03/CharSequence/src/Gnirts.java
@@ -2,18 +2,22 @@ public class Gnirts implements CharSequence {
 
   String text;
 
+  public Gnirts(String text) {
+    this.text = text;
+  }
+
   @Override
   public int length() {
-    return 0;
+    return this.text.length();
   }
 
   @Override
   public char charAt(int index) {
-    return 0;
+    return this.text.charAt(this.text.length() - 1 - index);
   }
 
   @Override
   public CharSequence subSequence(int start, int end) {
-    return null;
+    return this.text.subSequence(start, end);
   }
 }
