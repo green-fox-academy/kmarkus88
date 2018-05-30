e77f6bf59c0c642d73bd901114497f22a607fa64
kmarkus88 <kimmelmarkus88@gmail.com>
2018-05-30 16:10:29 +0200

    CharSequence: CharSequence implement skeleton added

---
+++ week-04/day-03/CharSequence/src/Gnirts.java
@@ -0,0 +1,19 @@
+public class Gnirts implements CharSequence {
+
+  String text;
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public char charAt(int index) {
+    return 0;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return null;
+  }
+}
