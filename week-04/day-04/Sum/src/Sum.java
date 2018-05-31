66445ff63bea1df3ddc398abb4942257aec10e7c
kmarkus88 <kimmelmarkus88@gmail.com>
2018-05-31 16:36:49 +0200

    feat(Sum): empty test created

--- week-04/day-04/Sum/src/Sum.java
+++ week-04/day-04/Sum/src/Sum.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Sum {
+
+  public Integer sumElements(ArrayList<Integer> numbers) {
+    int sum = 0;
+    for (Integer number : numbers ) {
+      sum += number;
+    }
+    return sum;
+  }
 }
--- week-04/day-04/Sum/test/SumTest.java
+++ week-04/day-04/Sum/test/SumTest.java
@@ -1,7 +1,33 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class SumTest {
 
-  
+  @Test
+  public void should_sumElements() {
+    Sum sumOfTheElements = new Sum();
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    numbers.add(5);
+    Integer expectedResult = 15;
+
+    assertEquals(expectedResult, sumOfTheElements.sumElements(numbers));
+  }
+
+  @Test
+  public void with_empty_list() {
+    Sum sumOfTheElements = new Sum();
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    Integer expectedResult = 1;
+
+    assertEquals(expectedResult, sumOfTheElements.sumElements(numbers));
+  }
 
 }
