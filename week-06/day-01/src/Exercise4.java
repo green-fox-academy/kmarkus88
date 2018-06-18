00381c0d2cd0f8ef79d00e54f619dca72cd261d9
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-18 12:58:50 +0200

    Ex3 done(with void function)

---
+++ week-06/day-01/src/Exercise4.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+
+    System.out.println(getNumbersWhereSquaredSumIsBigger20(numbers));
+  }
+  private static List<Integer> getNumbersWhereSquaredSumIsBigger20(List<Integer> numbers) {
+    return numbers.stream()
+            .mapToInt(n -> n)
+            .map(n -> n * n)
+            .filter(n -> n > 20)
+            .boxed()
+            .collect(Collectors.toList());
+  }
+}
