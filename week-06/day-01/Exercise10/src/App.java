86fa9e293d8b350f6e4d05412bb081bc534f0f4d
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-18 15:32:57 +0200

    Ex10 created(Fox class created)

---
+++ week-06/day-01/Exercise10/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+  public static void main(String[] args) {
+
+    Fox fox1 = new Fox("Feri", "pallida", "green");
+    Fox fox2 = new Fox("Juli", "junior", "green");
+    Fox fox3 = new Fox("Erzsi", "pallida", "red");
+    Fox fox4 = new Fox("Peti", "senior", "black");
+    Fox fox5= new Fox("Abraham", "pallida", "green");
+  }
+}
