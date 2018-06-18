dbaec477827f4eea3e0e9ecfafc728a0e7bc2e93
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-18 15:15:11 +0200

    Ex8 modified, Ex9 created

--- week-06/day-01/src/Exercise8.java
+++ week-06/day-01/src/Exercise8.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Exercise8 {
   public static void main(String[] args) {
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+    System.out.println(getNumbersFrequency(numbers));
 
   }
-  private static Map<Integer, Integer> getNumbersFrequency(List<Integer> numbers) {
-    
+  private static Map<Integer, Long> getNumbersFrequency(List<Integer> numbers) {
+    return numbers.stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
   }
 }
---
+++ week-06/day-01/src/Exercise9.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise9 {
+  public static void main(String[] args) {
+
+    Character[] chars = {'k','u','t','y','a'};
+  }
+  private static String getCharsToString(Character[] chars) {
+    return chars.
+  }
+}
