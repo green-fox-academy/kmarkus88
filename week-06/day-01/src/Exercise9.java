0dbcd6612010b63aa08881c308df6fd423b9aebe
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-18 15:18:30 +0200

    Ex9 done (thx Juci)

--- week-06/day-01/src/Exercise9.java
+++ week-06/day-01/src/Exercise9.java
@@ -6,8 +6,12 @@ public class Exercise9 {
   public static void main(String[] args) {
 
     Character[] chars = {'k','u','t','y','a'};
+    System.out.println(getCharsToString(chars));
   }
   private static String getCharsToString(Character[] chars) {
-    return chars.
+    return Arrays.stream(chars)
+            .map(c -> Character.toString(c))
+            .reduce((text, word) -> text + word)
+            .get();
   }
 }
