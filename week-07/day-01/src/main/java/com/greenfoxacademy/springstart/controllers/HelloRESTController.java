dd316550a259a353591e1241c3819d2e05706c33
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-25 15:08:07 +0200

    Java Spring: AtomicLong

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,13 +4,15 @@ import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
+  private AtomicLong c = new AtomicLong(1);
 
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam("name") String name) {
-    Greeting greeting = new Greeting(1, "Hello " + name);
+    Greeting greeting = new Greeting(c.getAndIncrement(), "Hello " + name);
     return greeting;
   }
 }
