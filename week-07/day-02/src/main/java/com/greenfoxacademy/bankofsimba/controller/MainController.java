3f1519233142947ca5a3c24aa1510b59d59d842d
kmarkus88 <kimmelmarkus88@gmail.com>
2018-06-26 15:08:31 +0200

    BankAccount: BankAccountStat.index done

---
+++ week-07/day-02/src/main/java/com/greenfoxacademy/bankofsimba/controller/MainController.java
@@ -0,0 +1,49 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+  private List<BankAccount> bankAccounts;
+
+  public MainController() {
+    bankAccounts = new ArrayList<>();
+    BankAccount timonacc = new BankAccount("Timon", "4000", "mongoose");
+    BankAccount pumbaacc = new BankAccount("Pumba", "3234", "pig");
+    BankAccount mufasaacc = new BankAccount("Mufasa", "10000", "lion");
+    BankAccount simbaacc = new BankAccount("Simba", "5635", "lion");
+    BankAccount rafikiacc = new BankAccount("Rafiki", "216578", "monkey");
+    bankAccounts.add(timonacc);
+    bankAccounts.add(pumbaacc);
+    bankAccounts.add(mufasaacc);
+    bankAccounts.add(simbaacc);
+    bankAccounts.add(rafikiacc);
+  }
+
+  @GetMapping("/show")
+  public String show(Model model) {
+    BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
+    model.addAttribute("bankAccount", bankAccount);
+    return "show";
+  }
+
+  @GetMapping("/utext")
+  public String showTestText(Model model) {
+    String testText = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+    model.addAttribute("testText", testText);
+    return "utext";
+  }
+
+  @GetMapping("/bankaccount")
+  public String getBankAccount(Model model) {
+    model.addAttribute("bankAccounts", bankAccounts);
+    return "bankaccount";
+  }
+}
