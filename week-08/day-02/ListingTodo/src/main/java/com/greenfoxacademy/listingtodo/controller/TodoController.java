53b036eb193e57dea31e57c76b47cddb0d3c1c20
kmarkus88 <kimmelmarkus88@gmail.com>
2018-07-03 16:02:44 +0200

    Try with & without responseBody

--- week-08/day-02/ListingTodo/src/main/java/com/greenfoxacademy/listingtodo/controller/TodoController.java
+++ week-08/day-02/ListingTodo/src/main/java/com/greenfoxacademy/listingtodo/controller/TodoController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//ez a @RequestMapping ahhoz kell hogy az osszes oldal a localhost:8080todo oldalon jelenjen meg
 @RequestMapping("/todo")
 public class TodoController {
-
+  //ez pedig ahhoz hogy a pertodoperlist vagy pertodoper url-en jelenjen meg
   @GetMapping(value ={"/", "/list"})
-  @ResponseBody
+  //A @ResponseBody ahhoz kellett hogy html file nelkul irjak a localhostra
+  //@ResponseBody
     public String list (Model model) {
-    return "This is my first todo";
+    //a returnbe pedig jott a szoveg
+    return "index";
   }
 }
