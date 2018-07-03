566d3687a50120be585198f6467be4420992adcf
kmarkus88 <kimmelmarkus88@gmail.com>
2018-07-03 13:51:46 +0200

    ListingTodo TodoController

--- week-08/day-02/ListingTodo/src/main/java/com/greenfoxacademy/listingtodo/controller/TodoController.java
+++ week-08/day-02/ListingTodo/src/main/java/com/greenfoxacademy/listingtodo/controller/TodoController.java
@@ -3,13 +3,16 @@ package com.greenfoxacademy.listingtodo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/todo")
 public class TodoController {
 
-@GetMapping("/todo")
-  public String todo (Model model) {
-
-  return "todo";
+  @GetMapping(value ={"/", "/list"})
+  @ResponseBody
+    public String list (Model model) {
+    return "This is my first todo";
   }
 }
