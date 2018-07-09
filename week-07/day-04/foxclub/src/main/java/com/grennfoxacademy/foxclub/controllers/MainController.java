a0e6f1e5fb04ede1bbda5aba1cd914c74b51a4b6
kmarkus88 <kimmelmarkus88@gmail.com>
2018-07-09 17:46:58 +0200

    FoxClub xmlns added

--- week-07/day-04/foxclub/src/main/java/com/grennfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/grennfoxacademy/foxclub/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.grennfoxacademy.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -14,5 +15,10 @@ public class MainController {
   @GetMapping("/login")
   public String showLoginPage() {
     return "login";
+  }
+
+  @PostMapping("/login")
+  public String createLoginAccount() {
+    return ("login");
   }
 }
